3d01cfbe4d9f5063eb91f28a365d2144179c2319
Anton Rudacov <anton.rudacov@gmail.com>
2018-05-28 14:41:03 +0200

    Changed package name

--- src/main/java/com/greglturnquist/payroll/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.greglturnquist.payroll;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-
-}
